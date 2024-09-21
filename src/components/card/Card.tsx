3f520235c480e0f69b2bcc96a51ecd4b0d82fa35
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-09-21 11:25:00 +0300

    add layout

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { rem } from "polished";
+import { ICard } from "../../types";
+import { colors } from "../styles/theme";
+import Button from "./Button";
+
+const Card = ({
+  id,
+  image,
+  price,
+  title,
+  weight,
+  isAdded,
+  addToBasket,
+}: ICard) => {
+  return (
+    <CardContainer>
+      <CardImage src={`/images/cards/${image}.jpg`} alt={title} />
+      <CardPrice className="text-big">{price}₽</CardPrice>
+      <CardTitle className="text">{title}</CardTitle>
+      <CardWeight>
+        {weight >= 1000 ? weight / 1000 + "кг" : weight + "г."}
+      </CardWeight>
+      <Button
+        className={isAdded ? "_orange _added" : ""}
+        onClick={() =>
+          !isAdded &&
+          addToBasket &&
+          addToBasket({ id, image, price, title, weight })
+        }
+      >
+        {isAdded ? "Добавлено" : "Добавить"}
+      </Button>
+    </CardContainer>
+  );
+};
+
+const CardContainer = styled.article`
+  padding: ${rem(12)};
+  border-radius: ${rem(18)};
+  background: ${colors.white};
+`;
+
+const CardImage = styled.img`
+  width: 100%;
+  height: ${rem(220)};
+  border-radius: ${rem(12)};
+  object-fit: cover;
+`;
+
+const CardPrice = styled.p`
+  margin-block: ${rem(10)} ${rem(8)};
+`;
+
+const CardTitle = styled.p`
+  margin-bottom: ${rem(29)};
+`;
+
+const CardWeight = styled.p`
+  color: ${colors.silver};
+  margin-bottom: ${rem(6)};
+`;
+
+export default Card;
---
+++ src/components/card/Cards.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import Card from "./Card";
+import { rem } from "polished";
+import { ICard, ICards } from "../../types";
+
+const Cards = ({ selectedLabel, cards, addToBasket, addedItems }: ICards) => {
+	const filteredCards = cards.filter(({ category }) =>
+		selectedLabel ? category === selectedLabel : true
+	);
+
+	return (
+		<CardsContainer>
+			<CardsTitle className="h2">{selectedLabel || "Все"}</CardsTitle>
+			<>
+				{filteredCards.length === 0 ? (
+					<p>Ничего не найдено</p>
+				) : (
+				<CardsItems>
+					{filteredCards.map(({ id, image, price, title, weight }: ICard) => (
+						<CardsItem key={id}>
+							<Card
+							id={id}
+							image={image}
+							price={price}
+							title={title}
+							weight={weight}
+							addToBasket={addToBasket}
+							isAdded={addedItems?.includes(id)}
+							/>
+						</CardsItem>
+					))}
+				</CardsItems>
+				)}
+			</>
+		</CardsContainer>
+	);
+};
+
+const CardsContainer = styled.section`
+  	margin-bottom: ${rem(50)};
+`;
+
+const CardsTitle = styled.h2`
+	margin-bottom: ${rem(24)};
+`;
+
+const CardsItems = styled.ul`
+	display: grid;
+	gap: ${rem(30)};
+	grid-template-columns: repeat(3, minmax(${rem(300)}, 1fr));
+`;
+
+const CardsItem = styled.li``;
+
+export default Cards;
