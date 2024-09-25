9ab3053a42d9f55292b39622f6cf583efc9957d3
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-09-25 16:08:02 +0300

    add new file styles

---
+++ src/styles/ButtonStyles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { rem } from "polished";
+import { colors } from "../styles/theme";
+
+export const ButtonContainer = styled.button`
+  height: ${rem(40)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  font-size: ${rem(16)};
+  border-radius: ${rem(12)};
+  background: ${colors.gray};
+  transition: all 0.3s ease-in-out;
+  color: ${colors.black};
+
+  &:hover {
+    background: ${colors.lightOrange};
+    color: ${colors.white};
+    transition: all 0.3s ease-in-out;
+  }
+
+  &._orange {
+    background: ${colors.orange};
+    color: ${colors.white};
+    &:hover {
+      background: ${colors.darkOrange};
+    }
+  }
+
+  &._disabled {
+    pointer-events: none;
+    opacity: 0.2;
+  }
+
+  &._added {
+    pointer-events: none;
+  }
+`;
